325ebc32a72c26688ef80953a4c71fbfe2596818
csan21 <chrisswho@gmail.com>
2017-04-01 17:40:03 -0500

    Add reverse method and alindrome check

--- algorithms.rb
+++ algorithms.rb
@@ -1,5 +1,4 @@
 # fibonacci iterative
-
 def fib_it(num)
   array = [0, 1]
   count = 2
@@ -10,15 +9,21 @@ def fib_it(num)
   array[num]
 end
 
+
 # fibonacci recursion
-
-# def fib(num)
-#   return 0 if num == 0
-#   return 1 if num == 1
-#   fib(num - 1) + fib(num - 2)
-# end
-
 def fib(num)
   return num if num == 0 || num == 1
   fib(num - 1) + fib(num - 2)
+end
+
+
+# simple string reverse without .reverse
+def hard_reverse(str)
+  str.chars.reduce{ |x,y| y+x }
+end
+
+
+# palindrome check
+def palindrome?(str)
+  str == str.reverse
 end
