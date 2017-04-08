2f5ba2d6ed5e425554dd731d96e17abe7b5e580b
csan21 <chrisswho@gmail.com>
2017-04-08 16:30:34 -0500

    Add merge sort

--- algorithms.rb
+++ algorithms.rb
@@ -97,3 +97,22 @@ class Array
 end
 # arr = [34, 2, 1, 5,3]
 # p arr.quicksort
+
+
+# merge sort
+def merge_sort(array)
+  return array if array.size <= 1
+
+  left, right = array.each_slice((array.size/2).round).to_a
+
+  merge = -> (left, right) {
+    array = []
+    while left.size > 0 && right.size > 0
+      array << (left[0] < right[0] ? left.shift : right.shift)
+    end
+
+    return array = array + left + right
+  }
+
+  return merge.call merge_sort(left), merge_sort(right)
+end
